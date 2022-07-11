8a50259f6aa90a7ae27b65e7536ae2dc27adc545
Nikita <nikit.suvorov@gmail.com>
2022-07-11 21:12:32 +0300

    lesson_4

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,22 @@ function App() {
     { id: v1(), title: 'Angular', isDone: true },
     { id: v1(), title: 'Vue', isDone: false },
   ]);
-
   const [filter, setFilter] = useState<FilterValuesType>('all');
 
   const removeItems = (itemID: string) => {
     setItems(items.filter((item) => item.id !== itemID));
+  };
+
+  const changeFilter = (filter: FilterValuesType) => {
+    setFilter(filter);
+  };
+
+  const addItems = (title: string) => {
+    setItems([{ id: v1(), title, isDone: false }, ...items]);
+  };
+
+  const changeStatus = (itemID: string, isDone: boolean) => {
+    setItems(items.map((item) => (item.id === itemID ? { ...item, isDone: isDone } : item)));
   };
 
   let itemsFilter;
@@ -36,23 +47,17 @@ function App() {
       itemsFilter = items;
   }
 
-  const changeFilter = (filter: FilterValuesType) => {
-    setFilter(filter);
-  };
-
-  const addItems = (title: string) => {
-    setItems([{ id: v1(), title, isDone: false }, ...items]);
-  };
-
   //UI:
   return (
     <div className="App">
       <TodoList
+        filter={filter}
+        changeStatus={changeStatus}
+        changeFilter={changeFilter}
         tasks={itemsFilter}
         title={title}
         removeItems={removeItems}
         addItems={addItems}
-        changeFilter={changeFilter}
       />
     </div>
   );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -13,13 +13,27 @@ type TodoListPropsType = {
   removeItems: (itemID: string) => void;
   addItems: (title: string) => void;
   changeFilter: (filter: FilterValuesType) => void;
+  changeStatus: (itemID: string, isDone: boolean) => void;
+  filter: FilterValuesType;
 };
 
-const TodoList = ({ title, tasks, removeItems, addItems, changeFilter }: TodoListPropsType) => {
+const TodoList = ({
+  tasks,
+  title,
+  addItems,
+  removeItems,
+  changeFilter,
+  changeStatus,
+  filter,
+}: TodoListPropsType) => {
   const [valueTitle, setValueTitle] = useState('');
+  const [error, setError] = useState<boolean>(false);
 
-  const addedTask = () => {
-    addItems(valueTitle);
+  const onChangeAddedTask = () => {
+    const trimmedTitle = valueTitle.trim();
+    if (trimmedTitle) {
+      addItems(trimmedTitle);
+    } else setError(true);
     setValueTitle('');
   };
 
@@ -29,20 +43,30 @@ const TodoList = ({ title, tasks, removeItems, addItems, changeFilter }: TodoLis
 
   const onKeyDownAddTask = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      addedTask();
+      onChangeAddedTask();
     }
   };
 
-  const onValueTitleTask = (event: ChangeEvent<HTMLInputElement>) =>
+  const onChangeRemoveItem = (id: string) => {
+    return () => removeItems(id);
+  };
+
+  const onValueTitleTask = (event: ChangeEvent<HTMLInputElement>) => {
+    error && setError(false);
     setValueTitle(event.currentTarget.value);
+  };
 
   const listItems = tasks.length ? (
     tasks.map((obj) => {
-      const removeItem = () => removeItems(obj.id);
+      const onChangeStatus = (event: ChangeEvent<HTMLInputElement>) => {
+        changeStatus(obj.id, event.currentTarget.checked);
+      };
+
       return (
-        <li key={obj.id}>
-          <input type="checkbox" checked={obj.isDone} /> <span>{obj.title}</span>
-          <button onClick={removeItem}>X</button>
+        <li key={obj.id} className={obj.isDone ? 'isDone' : ''}>
+          <input onChange={onChangeStatus} type="checkbox" checked={obj.isDone} />{' '}
+          <span>{obj.title}</span>
+          <button onClick={onChangeRemoveItem(obj.id)}>X</button>
         </li>
       );
     })
@@ -54,14 +78,30 @@ const TodoList = ({ title, tasks, removeItems, addItems, changeFilter }: TodoLis
     <div>
       <h3>{title}</h3>
       <div>
-        <input value={valueTitle} onChange={onValueTitleTask} onKeyDown={onKeyDownAddTask} />
-        <button onClick={addedTask}>+</button>
+        <input
+          className={error ? 'error' : ''}
+          value={valueTitle}
+          onChange={onValueTitleTask}
+          onKeyDown={onKeyDownAddTask}
+        />
+        <button onClick={onChangeAddedTask}>+</button>
+        {error && <div style={{ color: 'tomato' }}>Title is required!!</div>}
       </div>
       <ul>{listItems}</ul>
       <div>
-        <button onClick={onChangeFilterHandler('all')}>All</button>
-        <button onClick={onChangeFilterHandler('active')}>Active</button>
-        <button onClick={onChangeFilterHandler('completed')}>Completed</button>
+        <button className={filter === 'all' ? 'active' : ''} onClick={onChangeFilterHandler('all')}>
+          All
+        </button>
+        <button
+          className={filter === 'active' ? 'active' : ''}
+          onClick={onChangeFilterHandler('active')}>
+          Active
+        </button>
+        <button
+          className={filter === 'completed' ? 'active' : ''}
+          onClick={onChangeFilterHandler('completed')}>
+          Completed
+        </button>
       </div>
     </div>
   );
